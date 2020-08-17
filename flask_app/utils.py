32f22e07402672bec244619b25ff1bbb8f2aa020
zimkjh <zimkjh@naver.com>
2020-08-17 21:20:22 +0900

    utils 파일 리팩토링

--- flask_app/utils.py
+++ flask_app/utils.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 report_url = "https://navercomp.wisereport.co.kr/v2/company/c1050001.aspx?cn=&cmp_cd="
+code_url = "https://finance.naver.com/item/main.nhn?code="
 consensus_url = "https://navercomp.wisereport.co.kr/company/ajax/c1050001_data.aspx?flag=2&finGubun=MAIN&frq=0" \
                 "&chartType=svg&cmp_cd= "
-code_url = "https://finance.naver.com/item/main.nhn?code="
 
 
 # base code
@@ -29,3 +29,36 @@ def getSoupFromReport(code):
 
 def getJsonFromConsensus(code):
     return requests.get(consensus_url + code).json()
+
+
+# stock name code
+def isETF(soup):
+    if len(soup.select(".e_summary")) == 0:
+        return False
+    return True
+
+
+def isETN(soup):
+    name = getName(soup)
+    if "ETN" in name:
+        return True
+    return False
+
+
+def isFirstByName(name):
+    if name[-1] == "우":
+        return True
+    if name[-2:] in ["우A", "우B", "우C"]:
+        return True
+    return False
+
+
+def isFirstBySoup(soup):
+    name = getName(soup)
+    return isFirstByName(name)
+
+
+def isStockByName(name):
+    if "ETF" in name or "ETN" in name:
+        return False
+    return isFirstByName(name)
