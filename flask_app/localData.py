9ff34a1fc4892079e1874cf39ba22463fc99e1cd
zimkjh <zimkjh@naver.com>
2020-08-23 22:27:54 +0900

    size3size10 file algorithm 추가

--- flask_app/localData.py
+++ flask_app/localData.py
@@ -7,16 +7,25 @@ import logging
 logging.getLogger().setLevel(logging.INFO)
 codeFileName = datetime.today().strftime("%Y%m%d") + "_code.db"
 sizeFileName = datetime.today().strftime("%Y%m%d") + "_size.db"
+goodFileName = datetime.today().strftime("%Y%m%d") + "_good.db"
 
 
 def atDayStart():
     if fileExist(codeFileName):
         logging.info("codeFile already Exist")
-        codeL = getCodeFile()
+        codeL = getCodeL()
     else:
         logging.info("new codeFile required")
         codeL = utils.getCodeL()
-        putCodeFile(codeL)
+        putCodeL(codeL)
+
+    if fileExist(sizeFileName):
+        logging.info("sizeFile already Exist")
+        size3, size10 = getSize3Size10()
+    else:
+        logging.info("new sizeFile required")
+        size3, size10 = utils.getSize3Size10()
+        putSize3Size10(size3, size10)
 
 
 def fileExist(fileName):
@@ -25,7 +34,7 @@ def fileExist(fileName):
     return False
 
 
-def getCodeFile():
+def getCodeL():
     conn = sqlite3.connect("data/" + codeFileName)
     c = conn.cursor()
     c.execute("SELECT code FROM codeL")
@@ -35,11 +44,31 @@ def getCodeFile():
     return codeL
 
 
-def putCodeFile(codeL):
+def putCodeL(codeL):
     conn = sqlite3.connect("data/" + codeFileName)
     c = conn.cursor()
     c.execute("CREATE TABLE codeL (code)")
     codeString = " ".join(["('" + x + "')," for x in codeL])[:-1]
     c.execute("INSERT INTO codeL VALUES " + codeString)
+    conn.commit()
+    conn.close()
+
+
+def getSize3Size10():
+    conn = sqlite3.connect("data/" + sizeFileName)
+    c = conn.cursor()
+    c.execute("SELECT size3, size10 FROM sizeL")
+    rows = c.fetchall()
+    conn.close()
+    size3, size10 = rows[0]
+    return size3, size10
+
+
+def putSize3Size10(size3, size10):
+    conn = sqlite3.connect("data/" + sizeFileName)
+    c = conn.cursor()
+    c.execute("CREATE TABLE sizeL (size3, size10)")
+    sizeString = "('" + str(size3) + "', '" + str(size10) + "')"
+    c.execute("INSERT INTO sizeL VALUES " + sizeString)
     conn.commit()
     conn.close()
