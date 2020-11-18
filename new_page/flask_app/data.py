6cfd39498b172591273340b36be089506684ba6e
kpsy20 <kpsy20@naver.com>
2020-11-18 15:54:00 +0900

    new_page update

--- new_page/flask_app/data.py
+++ new_page/flask_app/data.py
@@ -2,9 +2,9 @@ import sqlite3
 
 
 def setCodeList(data):
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('NameAndCode.db')
     c = conn.cursor()
-    c.execute("INSERT INTO fundCodeList VALUES('KKK')")
+    c.execute("INSERT INTO NameAndCode VALUES('HAM','BUM')")
     print("setCodeList complet e")
     conn.commit()
     c.close()
@@ -21,10 +21,7 @@ def makeNameAndCode(name, col):
         cols_insert = cols_insert + "'"+content+"'"+", "
     c.execute("CREATE TABLE IF NOT EXISTS " + name + " (" + cols[0:-2] + ")")
 
-    # print("INSERT INTO NameAndCode VALUES" + " (" + cols[0:-2] + ")")
-    c.execute("INSERT INTO NameAndCode VALUES" +
-              " (" + cols_insert[0:-2] + ")")
-    # c.execute("CREATE TABLE IF NOT EXISTS " + name + " (" + cols[0:-2] + ")")
+    #c.execute("INSERT INTO NameAndCode VALUES" + " (" + cols_insert[0:-2] + ")")
     conn.commit()
     c.close()
 
